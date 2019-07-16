912dc2f8244612d21d36f556a6f8077d96f05472
yusf@100uu.com.cn <renyushu2016@gmail.com>
2019-07-16 16:20:55 +0800

    html report

---
+++ Python3LearnNote/run_all.py
@@ -0,0 +1,10 @@
+import unittest
+
+start_path = '/Users/yushufeng/study/'
+suite = unittest.TestSuite()
+discover = unittest.defaultTestLoader.discover(start_dir=start_path, pattern='test*.py', top_level_dir=None)
+# print(discover)
+suite.addTests(discover)
+
+runner = unittest.TextTestRunner()
+runner.run(suite)
