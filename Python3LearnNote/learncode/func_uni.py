0b9e539b1eb81e809d2dc26e27b2269d80d0c3b2
stu <yusf@100uu.com.cn>
2019-08-15 19:26:41 +0800

    done

--- Python3LearnNote/learncode/func_uni.py
+++ Python3LearnNote/learncode/func_uni.py
@@ -2,14 +2,22 @@
     通用函数
 """
 
-def tracer(func, *args, **kwargs):
-    print('calling: {}'.format(func.__name__))
-    return func(*args, **kwargs)
+# def tracer(func, *args, **kwargs):
+#     print('calling: {}'.format(func.__name__))
+#     return func(*args, **kwargs)
+#
+#
+# def add(a, b, c, d):
+#     return a+b+c+d
+#
+#
+# s = tracer(add, 1,2,34,5)
+# print(s)
 
+import os
 
-def add(a, b, c, d):
-    return a+b+c+d
-
-
-s = tracer(add, 1,2,34,5)
-print(s)
+p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+case_path = os.path.join(p, 'case/login.yaml')
+# p1 = p.split('/')
+print(case_path)
+print(__file__)
--- ui_auto_learn/test05.py
+++ ui_auto_learn/test05.py
@@ -1,5 +1,3 @@
-# -*- encoding=utf8 -*-
-__author__ = "yushufeng"
 
 from airtest.core.api import *
 import time
@@ -10,7 +8,10 @@ from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
 
 start_app('com.movies.hydq')
-time.sleep(3)
+time.sleep(5)
+poco("com.android.packageinstaller:id/permission_allow_button").wait(5).click()
+poco("com.android.packageinstaller:id/permission_allow_button").wait(5).click()
+
 
 poco(text="我的").click()
 poco(text="点击登录/注册").click()
@@ -19,3 +20,6 @@ text("yuuuuu2016@163.com")
 text("12345678")
 time.sleep(3)
 poco("com.movies.hydq:id/login_bt").click()
+
+if poco('yuuuuu2016@163.com ').exists:
+    print('login is successful')
